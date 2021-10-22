2922de9a033fbc0db1e8d66038797c39d964c903
Deadpool <fisson.florian@gmail.com>
2021-10-22 15:02:11 +0200

    Second commit

--- app/src/main/java/com/example/garticmemeproject/FinActivity.java
+++ app/src/main/java/com/example/garticmemeproject/FinActivity.java
@@ -1,14 +1,14 @@
 package com.example.garticmemeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class FinActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //faire une intent et lancer l'intent
+        setContentView(R.layout.activity_fin);
     }
 }
