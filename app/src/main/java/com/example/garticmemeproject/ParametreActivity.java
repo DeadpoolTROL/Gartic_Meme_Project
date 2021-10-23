903ab724280f0fa8bd3970f5ad57769e5f1e81f3
Deadpool <fisson.florian@gmail.com>
2021-10-23 22:14:19 +0200

    commit 6
    seekbar

--- app/src/main/java/com/example/garticmemeproject/ParametreActivity.java
+++ app/src/main/java/com/example/garticmemeproject/ParametreActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ParametreActivity extends AppCompatActivity {
 
@@ -12,6 +14,28 @@ public class ParametreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametre);
+
+        SeekBar seekbar;
+        TextView Nbjoueurs;
+        seekbar = (SeekBar)findViewById(R.id.seekBar2);
+        Nbjoueurs = (TextView)findViewById(R.id.textView4);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
+            {
+                Nbjoueurs.setText("Nombre de Joueurs : " + String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         Button bouton;
         bouton = (Button) findViewById(R.id.button3);
