2cf9fc82836b99f766c57ac52f03d132661125f6
Deadpool <fisson.florian@gmail.com>
2021-10-23 11:49:46 +0200

    Troisieme commit
    création plusieurs classe pour différentier les elements

---
+++ app/src/main/java/com/example/garticmemeproject/ReglesActivity.java
@@ -0,0 +1,14 @@
+package com.example.garticmemeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReglesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regles);
+    }
+}
