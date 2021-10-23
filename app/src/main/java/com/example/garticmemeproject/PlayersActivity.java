ffb99c67f7be4ce4cbed3d0db9dedcdf7cac9eae
Deadpool <fisson.florian@gmail.com>
2021-10-23 12:49:40 +0200

    Commit 4
    bouton pour exit sur playersactivity

--- app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
+++ app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
@@ -2,7 +2,10 @@ package com.example.garticmemeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PlayersActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class PlayersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_players);
-    }
-}
+        Button bouton;
+        bouton = (Button) findViewById(R.id.button2);
+
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+
+            }
+        });
+    }}
--- app/src/main/java/com/example/garticmemeproject/ReglesActivity.java
+++ app/src/main/java/com/example/garticmemeproject/ReglesActivity.java
@@ -11,4 +11,5 @@ public class ReglesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regles);
     }
+    
 }
