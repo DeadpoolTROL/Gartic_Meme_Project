ba93321847b51ca05bbd5d1785dd547bec262739
Deadpool <fisson.florian@gmail.com>
2021-10-24 19:55:38 +0200

    Hide action bar

--- app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
+++ app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,6 +23,9 @@ public class CreationmemeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_creationmeme);
 //---------------------------------------Bouton---------------------------------------------------------------------------
 
--- app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
+++ app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
@@ -6,6 +6,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class PlayersActivity extends AppCompatActivity {
@@ -13,6 +15,9 @@ public class PlayersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_players);
 
         Button bouton;
