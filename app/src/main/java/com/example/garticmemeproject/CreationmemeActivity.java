25a80167ae01e6fe3261d82c1096f0dfc8aa0892
Deadpool <fisson.florian@gmail.com>
2021-10-24 11:05:48 +0200

    commit 9
    timer

--- app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
+++ app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
@@ -3,12 +3,70 @@ package com.example.garticmemeproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CreationmemeActivity extends AppCompatActivity {
+
+    private TextView time;
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliseconds = 90000; //1 min 30
+    private boolean timeRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creationmeme);
+//---------------------------------------Bouton---------------------------------------------------------------------------
+
+        Button bouton;
+        bouton = (Button) findViewById(R.id.buttoncreation);
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.exit(0);
+
+            }
+        });
+
+        time = (TextView)findViewById(R.id.time);
+        updateTimer();
+        startTimer();
     }
+
+//----------------------------------------Timer--------------------------------------------------------------------------
+
+    public void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMilliseconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftInMilliseconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+                System.exit(0);
+            }
+        }.start();
+    }
+
+    public void updateTimer(){
+        int minute = (int) timeLeftInMilliseconds / 60000;
+        int seconde = (int) timeLeftInMilliseconds % 60000 / 1000;
+
+        String timeleftText;
+
+        timeleftText = "" + minute;
+        timeleftText += ":";
+        if (seconde < 10) timeleftText += "0";
+        timeleftText += seconde;
+
+        time.setText(timeleftText);
+    }
+
 }
