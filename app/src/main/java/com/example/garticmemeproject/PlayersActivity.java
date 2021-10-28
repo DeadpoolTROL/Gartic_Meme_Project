675cd58a22b08054843fcb3c8878ac030097bcfe
Deadpool <fisson.florian@gmail.com>
2021-10-28 22:40:26 +0200

    base joueur en cours

--- app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
+++ app/src/main/java/com/example/garticmemeproject/PlayersActivity.java
@@ -17,6 +17,7 @@ import java.util.Random;
 
 public class PlayersActivity extends AppCompatActivity {
 
+    private int idjoueur;
     public static final String EXTRA_MESSAGE2 = "";
     public static final String EXTRA_PROFIL = "";
     public static final String EXTRA_JOUEUR = "";
@@ -28,21 +29,22 @@ public class PlayersActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_players);
 
+        Button bouton;
+        bouton = (Button) findViewById(R.id.buttonjoueur);
         Bundle bundle = getIntent().getExtras();
-        Intent intent2 = getIntent();
-        String message2 = intent2.getStringExtra(ParametreActivity.EXTRA_MESSAGE);
-
-        int nbj = bundle.getInt(ParametreActivity.EXTRA_JOUEUR,0);
+        String theme = bundle.getString(ParametreActivity.EXTRA_MESSAGE);
+        int nbjoueur = bundle.getInt(ParametreActivity.EXTRA_JOUEUR,0);
 
         ImageView imgView = (ImageView) findViewById(R.id.logo4);
-        Random rand = new Random();
-        int randprofil = rand.nextInt(10) + 1;
-        String imgName = "profil" + randprofil;
+        String imgName = "profil" + idjoueur;
         int id = getResources().getIdentifier(imgName, "drawable", getPackageName());
         imgView.setImageResource(id);
 
-        Button bouton;
-        bouton = (Button) findViewById(R.id.buttonjoueur);
+        for (idjoueur = 0 ; idjoueur<=nbjoueur ; idjoueur++){
+
+        }
+
+
         bouton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,8 +52,8 @@ public class PlayersActivity extends AppCompatActivity {
                         PlayersActivity.this,
                         CreationmemeActivity.class
                 );
-                intent.putExtra(EXTRA_MESSAGE2, message2);
-                intent.putExtra(EXTRA_PROFIL, randprofil);
+                intent.putExtra(EXTRA_MESSAGE2, theme);
+                intent.putExtra(EXTRA_PROFIL, idjoueur);
                 startActivity(intent);
             }
         });
