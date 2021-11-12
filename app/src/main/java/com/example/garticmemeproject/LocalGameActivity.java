3e22fe389af3c08e4c59b5f9d28e4b5cb3d9118f
JeanProfite <jeanprofite.ben@gmail.com>
2021-11-12 10:13:12 +0100

    ajout fragment + activity

---
+++ app/src/main/java/com/example/garticmemeproject/LocalGameActivity.java
@@ -0,0 +1,14 @@
+package com.example.garticmemeproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_game);
+    }
+}
