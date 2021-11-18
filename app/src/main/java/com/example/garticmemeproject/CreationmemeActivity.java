b9d0e4dd43dd65c4dd24934cd80e9c9246dc3a59
Deadpool <fisson.florian@gmail.com>
2021-11-18 23:11:22 +0100

    Meme ecriture

--- app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
+++ app/src/main/java/com/example/garticmemeproject/CreationmemeActivity.java
@@ -5,16 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.Random;
 
@@ -28,18 +26,17 @@ public class CreationmemeActivity extends AppCompatActivity {
     private TextView time;
     private ImageView imageView;
     private CountDownTimer countDownTimer;
-    private long timeLeftInMilliseconds = 5000; //1 min 40      100000
+    private long timeLeftInMilliseconds = 5000000; //1 min 40      100000
     public static final String EXTRA_MESSAGE3 = "";
     public static final String EXTRA_MEME = "";
     private int progress = 100;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_creationmeme);
+        setContentView(R.layout.activity_creationmeme+randmeme);
 
         data3 = getIntent().getExtras();
 
@@ -49,10 +46,10 @@ public class CreationmemeActivity extends AppCompatActivity {
 
 //______________________________Image MEME__________________________________________________________________
 
-        ImageView imgMeme = (ImageView) findViewById(R.id.meme);
-        String imgName = "meme" + randmeme;
-        int id = getResources().getIdentifier(imgName, "drawable", getPackageName());
-        imgMeme.setImageResource(id);
+        //ImageView imgMeme = (ImageView) findViewById(R.id.meme);
+        //String imgName = "meme" + randmeme;
+        //int id = getResources().getIdentifier(imgName, "drawable", getPackageName());
+        //imgMeme.setImageResource(id);
 
 //______________________________Image Profil__________________________________________________________________
 
@@ -77,6 +74,23 @@ public class CreationmemeActivity extends AppCompatActivity {
                 intent.putExtras(data3);
                 if (countDownTimer != null){
                     countDownTimer.cancel();
+                }
+
+                EditText editText1 = (EditText) findViewById(R.id.ecriture1);
+                String ecriture1 = editText1.getText().toString();
+                data3.putString("ECRITURE1", ecriture1);
+                if (randmeme==1 || randmeme==3 || randmeme==5){
+                    EditText editText2 = (EditText) findViewById(R.id.ecriture2);
+                    String ecriture2 = editText2.getText().toString();
+                    data3.putString("ECRITURE2", ecriture2);
+                }
+                if (randmeme==4 || randmeme==6){
+                    EditText editText2 = (EditText) findViewById(R.id.ecriture2);
+                    String ecriture2 = editText2.getText().toString();
+                    EditText editText3 = (EditText) findViewById(R.id.ecriture3);
+                    String ecriture3 = editText3.getText().toString();
+                    data3.putString("ECRITURE2", ecriture2);
+                    data3.putString("ECRITURE3", ecriture3);
                 }
                 intent.putExtras(data3);
                 startActivity(intent);
@@ -103,6 +117,22 @@ public class CreationmemeActivity extends AppCompatActivity {
                         CreationmemeActivity.this,
                         VoteActivity.class
                 );
+                EditText editText1 = (EditText) findViewById(R.id.ecriture1);
+                String ecriture1 = editText1.getText().toString();
+                data3.putString("ECRITURE1", ecriture1);
+                if (randmeme==1 || randmeme==3 || randmeme==5){
+                    EditText editText2 = (EditText) findViewById(R.id.ecriture2);
+                    String ecriture2 = editText2.getText().toString();
+                    data3.putString("ECRITURE2", ecriture2);
+                }
+                if (randmeme==4 || randmeme==6){
+                    EditText editText2 = (EditText) findViewById(R.id.ecriture2);
+                    String ecriture2 = editText2.getText().toString();
+                    EditText editText3 = (EditText) findViewById(R.id.ecriture3);
+                    String ecriture3 = editText3.getText().toString();
+                    data3.putString("ECRITURE2", ecriture2);
+                    data3.putString("ECRITURE3", ecriture3);
+                }
                 data3.putInt("MEME",randmeme);
                 intent.putExtras(data3);
                 startActivity(intent);
@@ -133,5 +163,16 @@ public class CreationmemeActivity extends AppCompatActivity {
         Random rand = new Random();
         int hazardnumber = rand.nextInt(6) + 1;
         return (hazardnumber);
+    }
+
+    public String ecriture1() {
+        EditText editText1 = (EditText) findViewById(R.id.ecriture1);
+        String ecriture1 = editText1.getText().toString();
+        return (ecriture1);
+    }
+    public String ecriture2() {
+        EditText editText2 = (EditText) findViewById(R.id.ecriture2);
+        String ecriture2 = editText2.getText().toString();
+        return (ecriture2);
     }
 }
--- app/src/main/java/com/example/garticmemeproject/VoteActivity.java
+++ app/src/main/java/com/example/garticmemeproject/VoteActivity.java
@@ -21,11 +21,12 @@ public class VoteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_vote);
 
         data4 = getIntent().getExtras();
+        int randmeme = getIntent().getIntExtra("MEME",1);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_vote+randmeme);
 
         String theme = getIntent().getStringExtra("THEME");
         TextView textView = findViewById(R.id.themevote);
@@ -34,11 +35,28 @@ public class VoteActivity extends AppCompatActivity {
 
 //______________________________Image MEME__________________________________________________________________
 
-        int randmeme = getIntent().getIntExtra("MEME",1);
-        ImageView imgMeme = (ImageView) findViewById(R.id.Meme);
-        String imgMM = "meme" + randmeme;
-        int id2 = getResources().getIdentifier(imgMM, "drawable", getPackageName());
-        imgMeme.setImageResource(id2);
+        //int randmeme = getIntent().getIntExtra("MEME",1);
+        //ImageView imgMeme = (ImageView) findViewById(R.id.Meme);
+        //String imgMM = "meme" + randmeme;
+        //int id2 = getResources().getIdentifier(imgMM, "drawable", getPackageName());
+        //imgMeme.setImageResource(id2);
+
+        String ecriture1 = data4.getString("ECRITURE1");
+        TextView textView1 = findViewById(R.id.ecriture1);
+        textView1.setText(ecriture1);
+        if (randmeme==1 || randmeme==3 || randmeme==5){
+            String ecriture2 = data4.getString("ECRITURE2");
+            TextView textView2 = findViewById(R.id.ecriture2);
+            textView2.setText(ecriture2);
+        }
+        if (randmeme==4 || randmeme==6){
+            String ecriture2 = data4.getString("ECRITURE2");
+            TextView textView2 = findViewById(R.id.ecriture2);
+            textView2.setText(ecriture2);
+            String ecriture3 = data4.getString("ECRITURE3");
+            TextView textView3 = findViewById(R.id.ecriture3);
+            textView3.setText(ecriture3);
+        }
 
 //---------------------------------------Bouton---------------------------------------------------------------------------
 
