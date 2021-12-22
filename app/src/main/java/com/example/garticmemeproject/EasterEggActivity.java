f6d4de6eb2f0ccfc8ee1a0fb2b007277e2d75511
JeanProfite <jeanprofite.ben@gmail.com>
2021-12-22 22:55:03 +0100

    Résolution des problèmes dans EasterEggActivity

--- app/src/main/java/com/example/garticmemeproject/EasterEggActivity.java
+++ app/src/main/java/com/example/garticmemeproject/EasterEggActivity.java
@@ -2,11 +2,10 @@ package com.example.garticmemeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -18,9 +17,8 @@ public class EasterEggActivity extends AppCompatActivity {
 
     private Bundle data8;
     Button bouton;
-    private ImageView LayoutPooltato;
-    private ImageView LayoutJeanProfite;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,47 +29,38 @@ public class EasterEggActivity extends AppCompatActivity {
         mp2 = MediaPlayer.create(this, R.raw.bruh);
         data8 = getIntent().getExtras();
 
-        bouton = (Button) findViewById(R.id.buttonsecret);
-        bouton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(
-                        EasterEggActivity.this,
-                        FinActivity.class
-                );
-                intent.putExtras(data8);
-                startActivity(intent);
-            }
+        bouton = findViewById(R.id.buttonsecret);
+        bouton.setOnClickListener(v -> {
+            Intent intent = new Intent(
+                    EasterEggActivity.this,
+                    FinActivity.class
+            );
+            intent.putExtras(data8);
+            startActivity(intent);
         });
 
-        LayoutPooltato = findViewById(R.id.Pooltatopic);
-        LayoutPooltato.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Intent intent = new Intent(
-                        EasterEggActivity.this,
-                        PooltatoActivity.class
-                );
-                intent.putExtras(data8);
-                mp1.start();
-                startActivity(intent);
-                return false;
-            }
+        ImageView layoutPooltato = findViewById(R.id.Pooltatopic);
+        layoutPooltato.setOnTouchListener((view, motionEvent) -> {
+            Intent intent = new Intent(
+                    EasterEggActivity.this,
+                    PooltatoActivity.class
+            );
+            intent.putExtras(data8);
+            mp1.start();
+            startActivity(intent);
+            return false;
         });
 
-        LayoutJeanProfite = findViewById(R.id.JeanProfitepic);
-        LayoutJeanProfite.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Intent intent = new Intent(
-                        EasterEggActivity.this,
-                        JeanProfiteActivity.class
-                );
-                intent.putExtras(data8);
-                mp2.start();
-                startActivity(intent);
-                return false;
-            }
+        ImageView layoutJeanProfite = findViewById(R.id.JeanProfitepic);
+        layoutJeanProfite.setOnTouchListener((view, motionEvent) -> {
+            Intent intent = new Intent(
+                    EasterEggActivity.this,
+                    JeanProfiteActivity.class
+            );
+            intent.putExtras(data8);
+            mp2.start();
+            startActivity(intent);
+            return false;
         });
     }
 }
