73575899935f8e7c5502dc4618808f3a5b893263
JeanProfite <jeanprofite.ben@gmail.com>
2021-12-22 22:53:19 +0100

    Résolution des problèmes dans CurlyActivity

--- app/src/main/java/com/example/garticmemeproject/CurlyActivity.java
+++ app/src/main/java/com/example/garticmemeproject/CurlyActivity.java
@@ -2,40 +2,36 @@ package com.example.garticmemeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
 
 public class CurlyActivity extends AppCompatActivity {
 
-    private MediaPlayer mp = null;
     private Bundle dataCurly;
     ImageView Layout;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_curly);
 
-        mp = MediaPlayer.create(this, R.raw.ta_pas_damis_prend_un_curly_flo);
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.ta_pas_damis_prend_un_curly_flo);
         mp.start();
         dataCurly = getIntent().getExtras();
 
         Layout = findViewById(R.id.Curly);
-        Layout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Intent intent = new Intent(
-                        CurlyActivity.this,
-                        PlayersActivity.class
-                );
-                intent.putExtras(dataCurly);
-                startActivity(intent);
-                return false;
-            }
+        Layout.setOnTouchListener((view, motionEvent) -> {
+            Intent intent = new Intent(
+                    CurlyActivity.this,
+                    PlayersActivity.class
+            );
+            intent.putExtras(dataCurly);
+            startActivity(intent);
+            return false;
         });
     }
 }
