26fa129d33f114307efca1c488ec6a4dccc226b6
JeanProfite <jeanprofite.ben@gmail.com>
2021-12-23 20:04:24 +0100

    Résolution des problèmes dans ParametreActivity

--- app/src/main/java/com/example/garticmemeproject/ParametreActivity.java
+++ app/src/main/java/com/example/garticmemeproject/ParametreActivity.java
@@ -1,27 +1,24 @@
 package com.example.garticmemeproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class ParametreActivity extends AppCompatActivity {
 
-    private Bundle data = new Bundle();
+    private final Bundle data = new Bundle();
     private int nbjoueur = 1;
     private SeekBar seekbar;
     private TextView Nbjoueurs;
-    private int idjoueur = 0;
-    private int idjoueur2 = 0;
+    private final int idjoueur = 0;
+    private final int idjoueur2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +27,14 @@ public class ParametreActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_parametre);
 
-        seekbar = (SeekBar)findViewById(R.id.seekBar);
-        Nbjoueurs = (TextView)findViewById(R.id.nbjoueurtext);
+        seekbar = findViewById(R.id.seekBar);
+        Nbjoueurs = findViewById(R.id.nbjoueurtext);
 
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean b)
             {
-                Nbjoueurs.setText("Nombre de Joueurs : " + String.valueOf(progress));
+                Nbjoueurs.setText("Nombre de Joueurs : " + progress);
             }
 
             @Override
@@ -52,37 +49,33 @@ public class ParametreActivity extends AppCompatActivity {
         });
 
         Button bouton;
-        bouton = (Button) findViewById(R.id.buttonsetting);
-        bouton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                nbjoueur = seekbar.getProgress();
-                EditText editText = (EditText) findViewById(R.id.EditTheme);
-                String theme = editText.getText().toString();
+        bouton = findViewById(R.id.buttonsetting);
+        bouton.setOnClickListener(v -> {
+            nbjoueur = seekbar.getProgress();
+            EditText editText = findViewById(R.id.EditTheme);
+            String theme = editText.getText().toString();
 
-                data.putString("THEME", theme);
-                data.putInt("IDJOUEUR", idjoueur);
-                data.putInt("IDJOUEUR2", idjoueur2);
-                data.putInt("NBJOUEUR", nbjoueur);
+            data.putString("THEME", theme);
+            data.putInt("IDJOUEUR", idjoueur);
+            data.putInt("IDJOUEUR2", idjoueur2);
+            data.putInt("NBJOUEUR", nbjoueur);
 
-                if (nbjoueur == 1){
-                    Intent intent = new Intent(
-                            ParametreActivity.this,
-                            CurlyActivity.class
-                    );
-                    intent.putExtras(data);
-                    startActivity(intent);
-                }
-                else{
-                    Intent intent = new Intent(
-                            ParametreActivity.this,
-                            PlayersActivity.class
-                    );
-                    intent.putExtras(data);
-                    startActivity(intent);
-                }
-
+            Intent intent;
+            if (nbjoueur == 1){
+                intent = new Intent(
+                        ParametreActivity.this,
+                        CurlyActivity.class
+                );
             }
+            else{
+                intent = new Intent(
+                        ParametreActivity.this,
+                        PlayersActivity.class
+                );
+            }
+            intent.putExtras(data);
+            startActivity(intent);
+
         });
     }
 }
