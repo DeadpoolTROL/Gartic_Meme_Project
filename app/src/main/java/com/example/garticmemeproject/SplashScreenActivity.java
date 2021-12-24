e4c1509f7fa70642d48c084d6270a02d30005ef8
JeanProfite <jeanprofite.ben@gmail.com>
2021-12-24 16:31:22 +0100

    Ajout commentaires SplashScreenActivity

--- app/src/main/java/com/example/garticmemeproject/SplashScreenActivity.java
+++ app/src/main/java/com/example/garticmemeproject/SplashScreenActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+//Ici on affiche au lancement de l'application et pendant 2 secondes le splashscreen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
