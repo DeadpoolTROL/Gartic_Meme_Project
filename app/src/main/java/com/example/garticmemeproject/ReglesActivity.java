2b0e5b8609fd0d043017985a458e0850c738d086
JeanProfite <jeanprofite.ben@gmail.com>
2021-12-24 15:17:36 +0100

    Ajout commentaires ReglesActivity

--- app/src/main/java/com/example/garticmemeproject/ReglesActivity.java
+++ app/src/main/java/com/example/garticmemeproject/ReglesActivity.java
@@ -15,10 +15,11 @@ public class ReglesActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_regles);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //On enleve la barre horizontale en haut de l'écran
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); //On met en plein écran
+        setContentView(R.layout.activity_pooltato); //affichage du layout nommé activity_pooltato
 
+//Ici on affiche le layout activity_regles, lorsque le joueur appuie sur l'écran on passe a ParametreActivity
         ImageView layout = findViewById(R.id.touchregle);
         layout.setOnTouchListener((view, motionEvent) -> {
             Intent intent = new Intent(
